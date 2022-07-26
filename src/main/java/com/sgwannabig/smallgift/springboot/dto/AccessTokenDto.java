84e3d4daece50226fdf7eeebd289c459f9e638c1
이대로 <leedero01@naver.com>
2022-07-26 20:38:09 +0900

    dto update

--- src/main/java/com/sgwannabig/smallgift/springboot/dto/AccessTokenDto.java
+++ src/main/java/com/sgwannabig/smallgift/springboot/dto/AccessTokenDto.java
@@ -1,14 +1,17 @@
 package com.sgwannabig.smallgift.springboot.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AccessTokenDto {
     private String accessToken;
-
-    @Builder
-    public AccessTokenDto(String accessToken){
-        this.accessToken = accessToken;
-    }
+    private String token_type;
+    private String refresh_token;
+    private long expires_in;
+    private long refresh_token_expires_in;
 }
